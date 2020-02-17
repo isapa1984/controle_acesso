83fd531611150a08caa3f2bcc4705b361237ac7a
Igor Pastor <igordsouza@gmail.com>
2020-02-17 08:48:16 -0400

    Adiçao dos Models

---
+++ gestao_acesso/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GestaoAcessoConfig(AppConfig):
+    name = 'gestao_acesso'
---
+++ gestao_acesso/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Sistema(models.Model):
+    nome = models.CharField(max_length=250)
+    sigla = models.CharField(max_length=10)
+
+class Perfil(models.Model):
+    nome = models.CharField(max_length=250)
+    sistema = models.ForeignKey("Sistema", on_delete=models.CASCADE)
+
+class Acao(models.Model):
+    perfil = models.ForeignKey("Perfil", on_delete=models.CASCADE)
+    nome = models.CharField(max_length=250)
+    identificador = models.CharField(max_length=50)
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=250)
+    sexo = models.CharField(max_length=1)
+    data_nascimento = models.DateField()
+
+class Usuario(models.Model):
+    login = models.CharField(max_length=50)
+    senha = models.CharField(max_length=250)
+    perfis = models.ManyToManyField("Perfil")
