9b60bc08d63a6a7be5d65d344f074b9e2fcf2120
Igor Pastor <igordsouza@gmail.com>
2020-02-19 14:28:38 -0400

    inclusão de views

--- gestao_acesso/models.py
+++ gestao_acesso/models.py
@@ -4,19 +4,26 @@ class Sistema(models.Model):
     nome = models.CharField(max_length=250)
     sigla = models.CharField(max_length=10)
 
+    def __str__(self):
+        return f"{self.sigla} - {self.nome}"
+
+
 class Perfil(models.Model):
     nome = models.CharField(max_length=250)
     sistema = models.ForeignKey("Sistema", on_delete=models.CASCADE)
+
 
 class Acao(models.Model):
     perfil = models.ForeignKey("Perfil", on_delete=models.CASCADE)
     nome = models.CharField(max_length=250)
     identificador = models.CharField(max_length=50)
 
+
 class Pessoa(models.Model):
     nome = models.CharField(max_length=250)
     sexo = models.CharField(max_length=1)
     data_nascimento = models.DateField()
+
 
 class Usuario(models.Model):
     login = models.CharField(max_length=50)
--- gestao_acesso/views.py
+++ gestao_acesso/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from gestao_acesso.models import Sistema
 
 # Create your views here.
+
+def about(request):
+    sistemas = Sistema.objects.all()
+    str_list = ''.join(f'<li>{sistema.nome}</li>' for sistema in sistemas)
+    return HttpResponse(f'Sistema de Gestão de Acessos<br><br>Lista de Sistemas <ul>{str_list}</ul>')
